75e37d5e83fedddbd17721d375a887fe2cf0197c
TimisRobert <roberttimis@proton.me>
2024-02-20 22:56:16 +0100

    phoenix template

--- templates/phoenix/flake.nix
+++ templates/phoenix/flake.nix
@@ -20,15 +20,30 @@
         lib,
         config,
         ...
-      }: {
+      }: let
+        elixir = pkgs.beam.packages.erlangR26.elixir_1_16;
+        elixir-ls = (pkgs.elixir-ls.override {inherit elixir;}).overrideAttrs {
+          buildPhase = ''
+            runHook preBuild
+            mix do compile --no-deps-check, elixir_ls.release2
+            runHook postBuild
+          '';
+        };
+      in {
         devenv.shells.default = {
           packages = [
             pkgs.inotify-tools
-            pkgs.elixir
+            elixir
+            elixir-ls
           ];
 
           services.postgres = {
             enable = true;
+            package = pkgs.postgresql_16;
+            initialDatabases = [{name = "";}];
+            initialScript = ''
+              create user postgres superuser;
+            '';
             settings = {
               unix_socket_directories = lib.mkForce config.devenv.shells.default.env.PGDATA;
             };
