37122557bfd588c75e2e9fc15b07b1edbc3c3351
RobertTimis <roberttimis@gmail.com>
2024-03-15 00:12:48 +0100

    update templates

--- templates/elixir/flake.nix
+++ templates/elixir/flake.nix
@@ -2,6 +2,9 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
+    lexical.url = "github:lexical-lsp/lexical";
+    lexical.inputs.nixpkgs.follows = "nixpkgs";
+
     devenv.url = "github:cachix/devenv";
     devenv.inputs.nixpkgs.follows = "nixpkgs";
   };
@@ -16,14 +19,14 @@
       systems = nixpkgs.lib.systems.flakeExposed;
 
       perSystem = {pkgs, ...}: let
-        elixir = pkgs.beam.packages.erlangR26.elixir_1_16;
         devenvEnv = config.devenv.shells.default.env;
       in {
         devenv.shells.default = {
           env.MIX_HOME = "${devenvEnv.DEVENV_STATE}/.mix";
 
           packages = [
-            elixir
+            inputs'.lexical.packages.default
+            pkgs.beam.packages.erlangR26.elixir_1_16
           ];
         };
       };
--- templates/phoenix/flake.nix
+++ templates/phoenix/flake.nix
@@ -2,6 +2,9 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
+    lexical.url = "github:lexical-lsp/lexical";
+    lexical.inputs.nixpkgs.follows = "nixpkgs";
+
     devenv.url = "github:cachix/devenv";
     devenv.inputs.nixpkgs.follows = "nixpkgs";
   };
@@ -21,14 +24,14 @@
         config,
         ...
       }: let
-        elixir = pkgs.beam.packages.erlangR26.elixir_1_16;
         devenvEnv = config.devenv.shells.default.env;
       in {
         devenv.shells.default = {
           env.MIX_HOME = "${devenvEnv.DEVENV_STATE}/.mix";
 
           packages = [
-            elixir
+            inputs'.lexical.packages.default
+            pkgs.beam.packages.erlangR26.elixir_1_16
             pkgs.inotify-tools
           ];
 
