9c89f80b51bc0048764bba2dcd5e0e56fb699628
TimisRobert <roberttimis@proton.me>
2024-04-01 19:36:32 +0200

    update

--- modules/system/desktop/default.nix
+++ modules/system/desktop/default.nix
@@ -1,7 +1,5 @@
 {inputs, ...}: {
   home-manager = {
-    useGlobalPkgs = true;
-    useUserPackages = true;
     extraSpecialArgs = {inherit inputs;};
     users.rob = import ../../home/desktop;
   };
