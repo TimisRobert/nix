dcd6c8c596fe4adc9bdf16ae1ad0eebdff5d92a6
TimisRobert <roberttimis@proton.me>
2024-09-16 12:11:43 +0200

    chore: update

--- modules/home/desktop/default.nix
+++ modules/home/desktop/default.nix
@@ -1,7 +1,14 @@
-{...}: {
+{pkgs, ...}: {
   home.persistence."/persist/home/rob" = {
     directories = [
       ".local/share/Steam"
+    ];
+  };
+
+  programs.obs-studio = {
+    enable = true;
+    plugins = [
+      pkgs.obs-studio-plugins.wlrobs
     ];
   };
 
--- modules/system/desktop/default.nix
+++ modules/system/desktop/default.nix
@@ -1,6 +1,7 @@
 {
   lib,
   pkgs,
+  config,
   ...
 }: {
   nixpkgs.config.allowUnfreePredicate = pkg:
@@ -18,6 +19,12 @@
       pkgs.proton-ge-bin
     ];
   };
+
+  boot.extraModulePackages = [config.boot.kernelPackages.v4l2loopback];
+  boot.kernelModules = ["v4l2loopback"];
+  boot.extraModprobeConfig = ''
+    options v4l2loopback devices=1 video_nr=1 card_label="OBS Cam" exclusive_caps=1
+  '';
 
   networking = {
     hostId = "d5a63149";
