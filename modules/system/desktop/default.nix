850eba2b603f6f3f68125f8ee3aae8efe04237c8
TimisRobert <roberttimis@proton.me>
2025-01-26 20:49:29 +0100

    chore: zrepl only on desktop

--- modules/system/desktop/default.nix
+++ modules/system/desktop/default.nix
@@ -29,4 +29,89 @@
     hostId = "d5a63149";
     hostName = "desktop";
   };
+
+  services.zrepl = {
+    enable = true;
+    settings = {
+      jobs = [
+        {
+          name = "backuppool_sink";
+          type = "sink";
+          root_fs = "backuppool";
+          recv = {
+            placeholder = {
+              encryption = "inherit";
+            };
+          };
+          serve = {
+            type = "local";
+            listener_name = "backuppool_sink";
+          };
+        }
+        {
+          name = "push_to_drive";
+          type = "push";
+          send = {
+            encrypted = false;
+          };
+          connect = {
+            type = "local";
+            listener_name = "backuppool_sink";
+            client_identity = config.networking.hostName;
+          };
+          filesystems = {
+            "zpool/projects" = true;
+            "zpool/documents" = true;
+          };
+          replication = {
+            protection = {
+              initial = "guarantee_resumability";
+              incremental = "guarantee_incremental";
+            };
+          };
+          snapshotting = {
+            type = "manual";
+          };
+          pruning = {
+            keep_sender = [
+              {
+                type = "regex";
+                regex = ".*";
+              }
+            ];
+            keep_receiver = [
+              {
+                type = "grid";
+                grid = "1x1h(keep=all) | 24x1h | 14x1d | 3x30d";
+                regex = "^zrepl_.*";
+              }
+            ];
+          };
+        }
+        {
+          name = "snapshot";
+          type = "snap";
+          filesystems = {
+            "zpool/projects" = true;
+            "zpool/documents" = true;
+          };
+          snapshotting = {
+            type = "periodic";
+            prefix = "zrepl_";
+            interval = "30m";
+            timestamp_format = "iso-8601";
+          };
+          pruning = {
+            keep = [
+              {
+                type = "grid";
+                grid = "1x1h(keep=all) | 24x1h | 14x1d | 3x30d";
+                regex = "^zrepl_.*";
+              }
+            ];
+          };
+        }
+      ];
+    };
+  };
 }
