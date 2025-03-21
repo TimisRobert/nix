6a78a8155debabb40c2beb26610b7a1055742bbe
TimisRobert <roberttimis@proton.me>
2025-03-22 00:47:05 +0100

    chore: ply

--- modules/home/laptop/default.nix
+++ modules/home/laptop/default.nix
@@ -1,12 +1,22 @@
-{lib, ...}: {
+{...}: {
   imports = [../.];
 
-  wayland.windowManager.sway.config.keybindings = lib.mkOptionDefault {
-    "XF86MonBrightnessDown" = "exec light -U 5";
-    "XF86MonBrightnessUp" = "exec light -A 5";
-    "XF86AudioRaiseVolume" = "exec pamixer -i 5";
-    "XF86AudioLowerVolume" = "exec pamixer -d 5";
-    "XF86AudioMicMute" = "exec pamixer --source 111 -t";
-    "XF86AudioMute" = "exec pamixer -t";
+  wayland.windowManager.hyprland.settings = {
+    bind = [
+      " , XF86AudioRaiseVolume, exec, pamixer -i 5 "
+      " , XF86AudioLowerVolume, exec, pamixer -d 5 "
+      " , XF86AudioMicMute, exec, pamixer --default-source -t"
+      " , XF86AudioMute, exec, pamixer -t"
+      " , XF86AudioPlay, exec, playerctl play-pause"
+      " , XF86AudioPause, exec, playerctl play-pause"
+      " , XF86AudioNext, exec, playerctl next"
+      " , XF86AudioPrev, exec, playerctl previous"
+      " , XF86MonBrightnessUp, exec, light -A 5"
+      " , XF86MonBrightnessDown, exec, light -U 5"
+    ];
+    gestures = {
+      workspace_swipe = true;
+      workspace_swipe_fingers = 3;
+    };
   };
 }
--- modules/system/default.nix
+++ modules/system/default.nix
@@ -67,22 +67,11 @@
     kernelParams = [
       "quiet"
       "splash"
-      "boot.shell_on_fail"
-      "loglevel=3"
-      "rd.systemd.show_status=false"
-      "rd.udev.log_level=3"
-      "udev.log_priority=3"
       "plymouth.use-simpledrm"
     ];
   };
 
   powerManagement.cpuFreqGovernor = "performance";
-
-  xdg.portal = {
-    enable = true;
-    wlr.enable = true;
-    config.common.default = "*";
-  };
 
   virtualisation = {
     containers.enable = true;
