b50b27f46ac2f96efb2d05c37bf6dff6884c5ab0
TimisRobert <roberttimis@proton.me>
2025-03-22 00:15:16 +0100

    chore: ply

--- modules/system/desktop/default.nix
+++ modules/system/desktop/default.nix
@@ -11,11 +11,7 @@
     extraCompatPackages = [pkgs.proton-ge-bin];
   };
 
-  boot.extraModulePackages = [config.boot.kernelPackages.v4l2loopback];
-  boot.kernelModules = ["v4l2loopback"];
-  boot.extraModprobeConfig = ''
-    options v4l2loopback devices=1 video_nr=1 card_label="OBS Cam" exclusive_caps=1
-  '';
+  boot.kernelModules = ["amdgpu"];
 
   hardware = {
     graphics = {
