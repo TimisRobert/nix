d9b5f04bcd039156501b239c261d221fa5566226
TimisRobert <roberttimis@proton.me>
2025-05-06 13:06:36 +0200

    chore: update

--- modules/system/default.nix
+++ modules/system/default.nix
@@ -78,7 +78,7 @@
   networking = {
     networkmanager.enable = true;
     nftables.enable = true;
-    nameservers = ["1.1.1.1" "2606:4700:4700::1111"];
+    # nameservers = ["1.1.1.1" "2606:4700:4700::1111"];
     firewall = {
       trustedInterfaces = ["br0" "lan"];
       checkReversePath = false;
