7734d62eb3909cef012557158077ba3fd5307a5e
TimisRobert <roberttimis@proton.me>
2025-10-26 13:08:20 +0100

    chore: update

--- modules/home/desktop/default.nix
+++ modules/home/desktop/default.nix
@@ -2,10 +2,6 @@
   imports = [../.];
 
   home = {
-    sessionVariables = {
-      PROTON_ENABLE_WAYLAND = "1";
-      PROTON_USE_EAC_LINUX = "1";
-    };
     packages = [
       pkgs.godot
       pkgs.blender
