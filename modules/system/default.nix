81aef2829a0077307a6e8fb049062141d8b5ac4b
TimisRobert <roberttimis@proton.me>
2025-10-27 19:23:48 +0100

    chore: update

--- modules/system/default.nix
+++ modules/system/default.nix
@@ -185,15 +185,6 @@
   };
 
   services = {
-    openssh = {
-      enable = true;
-      hostKeys = [
-        {
-          path = "/persist/ssh/ssh_host_ed25519_key";
-          type = "ed25519";
-        }
-      ];
-    };
     udev.packages = [pkgs.yubikey-personalization];
     pcscd.enable = true;
     tailscale.enable = true;
